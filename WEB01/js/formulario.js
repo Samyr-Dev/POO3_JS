7ab7476471348e81badbf903d6a894116c4c5bb7
Samyr Tertuliano <samyrtertuliano4@gmail.com>
2026-03-03 20:59:23 -0300

    Alterações no JS

--- WEB01/js/formulario.js
+++ WEB01/js/formulario.js
@@ -1,1 +1,17 @@
-console.log("Exercícios de JavaScript")
+let btnAdicionar = document.getElementById("btnAdicionar");
+
+function adicionarTarefa() {
+    const txtTarefa = document.getElementById("txtTarefa");
+    const texto = txtTarefa.value;
+    const liTarefa = document.getElementById("listaTarefas");
+    
+    if (texto) {
+        const novaTarefa = document.createElement("li");
+        const textoLi = document.createTextNode(texto.toUpperCase());
+        novaTarefa.appendChild(textoLi);
+        liTarefa.appendChild(novaTarefa);
+        txtTarefa.value = "";
+    }
+
+}
+
